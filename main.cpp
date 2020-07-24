503146f7c019a45cc30db7ddee34390c3112be26
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-23 23:02:31 -0300

    created project struct and draw de base game

--- main.cpp
+++ main.cpp
@@ -2,12 +2,73 @@
 
 using namespace std;
 
-int main() {
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+eDirection dir;
 
-    int a = 2;
-    int b = 3;
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
 
-    int result = (a+b);
+    score = 0;
+}
+void Draw()
+{
+    system("clear"); // if you using windows OS ghange this line for system("cls")
+    for (int i = 0; i < width + 1; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            else
+                cout << " ";
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 1; i++)
+        cout << "#";
+    cout << endl;
+}
+void Input()
+{
+}
+void Logic()
+{
+}
+int main()
+{
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10), sleep(10);
+    }
     return 0;
 }
-
